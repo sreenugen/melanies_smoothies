8da62c4c751ce06463210e25d6822ecc453aea4b
sreenugen <80684099+sreenugen@users.noreply.github.com>
2026-03-04 13:10:43 +0530

    Update streamlit_app.py
    modified the API code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -38,14 +39,13 @@ if ingredients_list:
 
     st.write(my_insert_stmt)
     #st.stop()
-    time_to_insert = st.button('Submit Order')
-
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)  
+    time_to_insert = st.button('Submit Order')  
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
         #st.success('Your Smoothie is ordered!', icon="✅")
         st.success(f"Your Smoothie is ordered, {name_on_order}!")
+
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
