c4899d25dc5944bd42b18860ca9830a14a955a9a
sreenugen <80684099+sreenugen@users.noreply.github.com>
2026-03-04 13:26:02 +0530

    Update streamlit_app.py
    updated the API call response on each chosen fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,10 @@ if ingredients_list:
         
         #st.success('Your Smoothie is ordered!', icon="✅")
         st.success(f"Your Smoothie is ordered, {name_on_order}!")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+if ingredients_list:
+  ingredients_string = ''
+  for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
